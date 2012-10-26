f4d7cd74d303266d5e3a47a1cdf97b253800862e
Franklin Castellanos <frank_nightmare@unitec.edu>
2012-10-26 04:45:40 -0600

    Primeros archivos

---
+++ src/Primer_Ejemplo/ClasePrueba.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Primer_Ejemplo;
+
+/**
+ *
+ * @author Franklin
+ */
+public class ClasePrueba {
+    public ClasePrueba(){
+        
+    }
+}
