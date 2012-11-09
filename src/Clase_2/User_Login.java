0068bc75bae0940e1c1989482271d539d223033a
Franklin Castellanos <frank_nightmare@unitec.edu>
2012-11-09 08:11:22 -0600

    Generar Objetos

--- src/Clase_2/User_Login.java
+++ src/Clase_2/User_Login.java
@@ -19,10 +19,11 @@ import javax.swing.JOptionPane;
  *
  * @author Franklin
  */
+
 public class User_Login extends javax.swing.JFrame {
 
-    private String nombre="Frank";
-    private String passw="Frank";
+    
+    JRegister registro=new JRegister();
     
     public User_Login() {
         initComponents();
@@ -44,11 +45,13 @@ public class User_Login extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         lblImagen = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 14));
         jLabel1.setForeground(new java.awt.Color(0, 0, 153));
         jLabel1.setText("Inicio Sesión");
         getContentPane().add(jLabel1);
@@ -58,12 +61,12 @@ public class User_Login extends javax.swing.JFrame {
         getContentPane().add(txtPassword);
         txtPassword.setBounds(140, 280, 140, 20);
 
-        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 11));
         jLabel2.setText("Password: ");
         getContentPane().add(jLabel2);
         jLabel2.setBounds(140, 260, 90, 14);
 
-        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 11));
         jLabel3.setText("Nombre: ");
         getContentPane().add(jLabel3);
         jLabel3.setBounds(140, 210, 100, 14);
@@ -83,25 +86,46 @@ public class User_Login extends javax.swing.JFrame {
                 jButton1MouseClicked(evt);
             }
         });
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
         getContentPane().add(jButton1);
         jButton1.setBounds(170, 330, 90, 30);
+
+        jLabel4.setFont(new java.awt.Font("Tahoma", 1, 14));
+        jLabel4.setText("Registrarse");
+        jLabel4.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel4MouseClicked(evt);
+            }
+        });
+        getContentPane().add(jLabel4);
+        jLabel4.setBounds(310, 340, 100, 30);
+
+        jLabel5.setFont(new java.awt.Font("Tahoma", 1, 14));
+        jLabel5.setText("View Users");
+        jLabel5.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel5MouseClicked(evt);
+            }
+        });
+        getContentPane().add(jLabel5);
+        jLabel5.setBounds(30, 340, 100, 30);
 
         java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
         setBounds((screenSize.width-430)/2, (screenSize.height-433)/2, 430, 433);
     }// </editor-fold>//GEN-END:initComponents
 
 private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
-    if(!this.txtNombre.getText().equals(this.nombre)){
-        JOptionPane.showMessageDialog(this,"El nombre "+this.txtNombre.getText()+" es inválido",
-                "Información incorrecta",JOptionPane.ERROR_MESSAGE);
-        
-    }else if(!this.txtPassword.getText().equals(this.passw)){
-        JOptionPane.showMessageDialog(this,"El password es inválido",
-                "Información incorrecta",JOptionPane.ERROR_MESSAGE);
+    if(this.registro.validarInfo(this.txtNombre.getText(),this.txtPassword.getText())){
+        JOptionPane.showMessageDialog(this, "Bienvenido","Felicitaciones",
+                JOptionPane.INFORMATION_MESSAGE);
     }else{
-        JOptionPane.showMessageDialog(this,"Ha iniciado sesión satisfactoriamente",
-                "Bienvenido",JOptionPane.INFORMATION_MESSAGE);
-    }    
+        JOptionPane.showMessageDialog(this,"La información no existe","Información inválida",
+                JOptionPane.ERROR_MESSAGE);
+    }
 }//GEN-LAST:event_jButton1MouseClicked
 
 private void lblImagenMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblImagenMouseClicked
@@ -115,10 +139,24 @@ private void lblImagenMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:e
         colocarImagen(temp);
     }else{
         JOptionPane.showMessageDialog(this, "No ha seleccionado ningún archivo","Información inválida",
-                JOptionPane.ERROR);
+                JOptionPane.ERROR_MESSAGE);
     }
     
 }//GEN-LAST:event_lblImagenMouseClicked
+
+private void jLabel4MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel4MouseClicked
+    registro.setVisible(true);
+    
+}//GEN-LAST:event_jLabel4MouseClicked
+
+private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    
+}//GEN-LAST:event_jButton1ActionPerformed
+
+private void jLabel5MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel5MouseClicked
+    List_Users vista=new List_Users(this.registro.getInfo());
+    vista.setVisible(true);
+}//GEN-LAST:event_jLabel5MouseClicked
 
     /**
      * @param args the command line arguments
@@ -160,6 +198,8 @@ private void lblImagenMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:e
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel lblImagen;
     private javax.swing.JTextField txtNombre;
     private javax.swing.JPasswordField txtPassword;
---
+++ src/Clase_2/Usuarios.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clase_2;
+
+/**
+ *
+ * @author Franklin
+ */
+public class Usuarios {
+    public String name,password,apellido,path;
+    public Usuarios(String name,String password,String apellido,String path){
+        this.name=name;
+        this.password=password;
+        this.path=path;
+        this.apellido=apellido;
+    }
+}
